c24fac17a2a8a04168d75b4edf68fe6f8af987d3
Nazmi Altun <nazmialtun@windowslive.com>
2020-05-08 15:58:22 +0800

    Refactor

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,14 +3,11 @@
 # project-key = $1
 # project-name = $2
 # organization = $3
-# version = $4
-# cpd-exclusions = $5
-# opencover-report-paths = $6
-# url = $7
-# solution-path = $8
-# pr-repository=$9
-# pr-branch=$10
-# pr-key=$11
+# cpd-exclusions = $4
+# opencover-report-paths = $5
+# url = $6
+# solution-path = $7
+# pr-key=$8
 
 set -eu
 
@@ -18,27 +15,27 @@ begin_cmd="/dotnet-sonarscanner begin \\
     /k:\"${1}\" \\
     /n:\"${2}\" \\
     /o:\"${3}\" \\
-    /d:sonar.host.url=\"${7}\" \\
-    /version:\"${4}\" \\
+    /d:sonar.host.url=\"${6}\" \\
+    /version:\"${GITHUB_RUN_NUMBER}\" \\
     /d:sonar.login=\"${SONAR_TOKEN:?Please set the SONAR_TOKEN environment variable.}\""
-
-if [ -n "$6" ]
-then
-    begin_cmd="$begin_cmd /d:sonar.cs.opencover.reportsPaths=\"${6}\""
-fi
 
 if [ -n "$5" ]
 then
-    begin_cmd="$begin_cmd /d:sonar.cpd.exclusions=\"${5}\""
+    begin_cmd="$begin_cmd /d:sonar.cs.opencover.reportsPaths=\"${5}\""
 fi
 
-if [ -n "$11" ]
+if [ -n "$4" ]
 then
-    begin_cmd="$begin_cmd /d:sonar.pullrequest.github.repository=\"${9}\" \\
-    /d:sonar.pullrequest.github.branch=\"${10}\" \\
-    /d:sonar.pullrequest.github.key=\"${11}\""
+    begin_cmd="$begin_cmd /d:sonar.cpd.exclusions=\"${4}\""
+fi
+
+if [ -n "$8" ]
+then
+    begin_cmd="$begin_cmd /d:sonar.pullrequest.github.repository=\"${GITHUB_REPOSITORY}\" \\
+    /d:sonar.pullrequest.github.branch=\"${GITHUB_REF#refs/heads/}\" \\
+    /d:sonar.pullrequest.github.key=\"${8}\""
 fi
 
 
-sh -c "dotnet build -c Release $8"
+sh -c "dotnet build -c Release $7"
 sh -c "dotnet-sonarscanner end /d:sonar.login=\"${SONAR_TOKEN}\""
